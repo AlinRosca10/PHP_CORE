a8a49a98895fc93e52d98497bbd190051beb740f
Alin <alin.rosca545@yahoo.com>
2025-10-14 18:03:29 +0300

    Reinitialized calculator space...

--- calculator.php
+++ calculator.php
@@ -2,11 +2,11 @@
     //localhost/EXERCITII_PHP/PHP/curs_1/calculator.php?x=3&y=10
     var_dump($_GET);
     echo "<br>";
-    $x=$_GET['x'];
-    $y=$_GET['y'];
-    $z=$_GET['z'];
+    $x = $_GET['x'];
+    $y = $_GET['y'];
+    $z = $_GET['z'];
 
-    $rez=$x+$y+$z;
+    $rez = $x+$y+$z;
     echo "Rezultatul este $rez";//interpolare de stringuri
     echo "<br>";
     echo 'Rezultatul este '.$rez;//contatenare performanta mai buna cu concatenarea decat cu interpolarea
