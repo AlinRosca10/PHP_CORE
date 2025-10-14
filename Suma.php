4a807f6f70403c5c7f618a91c963d0913a3e4289
Alin <alin.rosca545@yahoo.com>
2025-10-14 18:59:56 +0300

    Curs 3 reinitializat + fisiere auxiliare

---
+++ Suma.php
@@ -0,0 +1,9 @@
+<?php
+
+echo 'Numele fisierului este: '. $argv[0];
+echo "\n";
+echo "Suma este:". $argv[1]+$argv[2];
+echo "\n";
+
+?>
+
---
+++ echo1.php
@@ -0,0 +1,7 @@
+<?php
+    $array5=array(
+        'cursuri'=>array('php','html','java'),
+        'sali'=>array('sala1','sala2','sala3')
+    );
+    echo $array5['cursuri'][1]. ' '.$array5['sali'][2];
+?>
