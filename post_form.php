242a9132703d76094d4674cd0327acdb0b9d60a7
Alin <alin.rosca545@yahoo.com>
2025-10-15 17:56:31 +0300

    Aurora post authentification

--- post_form.php
+++ post_form.php
@@ -7,6 +7,7 @@
         <title>My first Login Form</title>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link href="maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" id="bootstrap-css">
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
         <div id="login">
@@ -15,17 +16,17 @@
                 <div id="login-row" class="row justify-content-center align-items-center">
                     <div id="login-column" class="col-md-6">
                         <div id="login-box" class="col-md-12">
-                            <form id="login-form" class="form" action="post_form.php" method="post">
-                                <h3 class="text-center text-info">Login</h3>
+                            <form id="login-form" class="form form-group" action="post_form.php" method="post">
+                                <h3 class="text-center text-info aurora-input">Login</h3>
                                 <div class="form-group">
-                                    <label for="username" class="text-info">Username:</label><br>
-                                    <input type="text" name="username" id="username" class="form-control"> 
+                                    <label for="username" class="text-info aurora-label">Username:</label><br>
+                                    <input type="text" name="username" id="username" class="form-control aurora-input"> 
                                 </div>
                                 <div class="form-group">
-                                    <label for="password" class="text-info">Password:</label><br>
-                                    <input type="password" name="password" id="password" class="form-control">
+                                    <label for="password" class="text-info aurora-label">Password:</label><br>
+                                    <input type="password" name="password" id="password" class="form-control aurora-input">
                                     <br>
-                                    <input type="submit" name="submit" class="btn btn-info btn-md" value="Submit">
+                                    <input type="submit" name="submit" class="btn btn-info btn-md aurora-button" value="Submit">
                                 </div>
                             </form>
                         </div>
