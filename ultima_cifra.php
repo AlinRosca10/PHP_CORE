2ea80a5d0fd74294b4944b123a38ae1a35d39474
Alin <alin.rosca545@yahoo.com>
2025-12-02 11:04:46 +0200

    Eliminating the logical error that could compromise the sum in the case of a negative number, changing the sign using an if-type decision structure

--- ultima_cifra.php
+++ ultima_cifra.php
@@ -2,6 +2,9 @@
     $number = $_GET['number'];
     echo "Suma cifrelor numarului $number este: ";
     $sum_number = 0;
+    if ($number < 0) {
+        $number = -$number;
+    }
     while ($number > 0) {
         $digit = $number % 10;
         $sum_number += $digit;
