ad7123211d8dd7ebb922a937d71106a8c122906d
Alin <alin.rosca545@yahoo.com>
2025-12-03 20:29:09 +0200

    Determinarea pozitiei unui element intr-un sir cand se folosesc tipuri diferite de ordonare

--- accesare_valoare_sir_array_vector.php
+++ accesare_valoare_sir_array_vector.php
@@ -1,2 +1,19 @@
 <?php
+    $a = 10;
+    $b = 5;
+    $c = $a + $b;
+    echo "Suma lui $a si $b este egala cu $c.";
+
+    echo "<hr>";
+    $fructe = array("mar", "banana", "cireasa");
+    echo "Primul fruct din lista este: " . $fructe[0];
+
+    echo "<hr>";
+
+    $a = array('a', 3 => 'b', 1 => 'c', 'd');
+    echo $a[2]; // va afisa 'd'
+    echo "<hr>";
+    for ($i = 0; $i < count($a); $i++) {
+        echo "Elementul $i este: " . $a[$i] . "<br>";
+    }
 ?>
