69cb655cc9581d14980482e44155623a0d0b6321
Alin <alin.rosca545@yahoo.com>
2025-12-03 17:12:50 +0200

    Atentia distributiva in cadrul codului evita o interpretare gresita prin capcane date de operatorul de atribuire care in structura decizionala este TRUE...

---
+++ structura_decizionala_capcana_interpretare.php
@@ -0,0 +1,17 @@
+<?php 
+    $x = 10;
+    if ($x = 5) {
+        echo 'Valoarea este 5'; // Atribuie valoarea 5 lui $x și afișează 'Valoarea este 5'
+    } elseif ($x == 10) {
+        echo 'Valoarea este 10'; // Afișează 'Valoarea este 10' dacă $x este egal cu 10
+    } else {
+        echo 'Altă valoare'; // Afișează 'Altă valoare' dacă niciuna dintre condițiile anterioare nu este adevărată
+    }
+
+    echo "<br>";
+
+    $x = 10;
+    if ($x == 25) {
+        echo "Valoarea lui $x este 25"; // Afișează 'Valoarea este 25' dacă $x este egal cu 5
+    }
+?>
