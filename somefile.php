8f86ed787a66937f9fc90112689abacf51b2ab81
Alin Rosca <alin.rosca545@yahoo.com>
2025-12-04 13:47:01 +0200

    Diferente la nivel structural intre require si require_once

---
+++ somefile.php
@@ -0,0 +1,7 @@
+<?php
+    echo "somefile.php has been required.<br>";
+    # End of somefile.php
+    $x = 10; // just to have some content
+    $y = 20; // just to have some content
+    $sum = $x + $y; // just to have some content
+?>
