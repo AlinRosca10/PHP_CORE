ab5afa3011e170935fc98db3d00f1f6f122dad6c
Alin Rosca <alin.rosca545@yahoo.com>
2025-12-16 19:07:55 +0200

    crearea unei functii anonime ce afiseaza o suma returna in interiorul ei

---
+++ functii/anonime/functie_anonima.php
@@ -0,0 +1,8 @@
+<?php
+    $x = 10;
+    $y = 23;
+    $adunare = function (int $x, int $y) {
+        return $x + $y;
+    };
+    echo $adunare(10, 23);
+?>
--- functii/suma_impunere_tipuri_date_functie_valoare_returnata.php
+++ functii/suma_impunere_tipuri_date_functie_valoare_returnata.php
@@ -1,4 +1,5 @@
 <?php
+    declare (strict_types=1);
     function suma(int $x, int $y) : int {
         return $x + $y;
     }
