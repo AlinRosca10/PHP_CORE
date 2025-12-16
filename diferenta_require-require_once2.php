8073dd4798a8beae937459fe33a539ce4eebf023
Alin Rosca <alin.rosca545@yahoo.com>
2025-12-16 11:55:17 +0200

    Rezumatul apare ca overlay când treci cu mouse-ul peste card, 1️⃣ obținând acum 🎯 : ✔ când planează mouse-ul (hover) pe carte, apare rezumatul ✔ efect smooth ✔ funcționează pentru toate cele 100 de cărți ✔ fără JavaScript

--- diferenta_require-require_once2.php
+++ diferenta_require-require_once2.php
@@ -6,6 +6,33 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <meta http-equiv="X-AU-Coopatible" content="ie=edge">
         <title>Lista carti generata din PHP</title>
+        <style>
+            .card {
+                position: relative;
+                overflow: hidden;
+            }
+
+            .card-summary {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 20%;
+                background: rgba(0, 0, 0, 0.85);
+                color: #fff;
+                padding: 20px;
+                opacity: 0;
+                transition: opacity 0.3s ease-in-out;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                text-align: center;
+            }
+
+            .card:hover .card-summary {
+                opacity: 1;
+            }
+        </style>
     </head>
     <body>
         <div class="container clearfix">
@@ -23,22 +50,27 @@
                     <div class="col-6 col-sm-4 col-md-4 col-lg-4 mt-2 ">
                         <div class="card">
                             <img class="card-img-top" style="height: 500px" src="<?php echo $book['image_url']; ?>" alt="Card image" style="width: 100V">
+                            <div class="card-summary">
+                                <p><?php echo $book['summary']; ?></p>
+                            </div>
                             <div class="card-body">
                                 <h4 class="card-title"><?php echo $book['title']; ?></h4>
                                 <h6 class="card-subtitle mb-2 text-muted"><?php echo $book['author']; ?>, <?php echo $book['country']; ?></h6>
+                                <br>
                                 <h5 class="card-subtitle mb-1 text-muted" style="text-align: right">Price: <?php echo number_format($book['price'], 2) . ' ' . $book['currency']; ?></h5>
                                 <br>
-                                <p class="card-text">Language: <?php echo $book['language']; ?>;
-                                                    Pages: <?php echo $book['pages']; ?>;
-                                                    Year: <?php echo $book['year']; ?>;
-                                                    Edition: <?php echo $book['edition']; ?>;
-                                                    Publisher: <?php echo $book['publisher']; ?>;
-                                                    ISBN: <?php echo $book['isbn']; ?>;
-                                                    Format: <?php echo $book['format']; ?>;
-                                                    Publisher City: <?php echo $book['publisher_city']; ?>;
-                                                    Publication date: <?php echo $book['publication_date']; ?>;
-                                                    Edition number: <?php echo $book['edition_number']; ?>;
-                                                    Genre: <?php echo $book['genre']; ?>
+                                <h7 class="card-subtitle mb-3 text-muted" style="text-align: right">Genre: <?php echo $book['genre']; ?></h7>
+                                <hr>
+                                <p class="card-text">Language: <?php echo $book['language']; ?>; 
+                                                    Pages: <?php echo $book['pages']; ?>; 
+                                                    Year: <?php echo $book['year']; ?>; 
+                                                    Edition: <?php echo $book['edition']; ?>; 
+                                                    Publisher: <?php echo $book['publisher']; ?>; 
+                                                    ISBN: <?php echo $book['isbn']; ?>; 
+                                                    Format: <?php echo $book['format']; ?>; 
+                                                    Publisher City: <?php echo $book['publisher_city']; ?>; 
+                                                    Publication date: <?php echo $book['publication_date']; ?>; 
+                                                    Edition number: <?php echo $book['edition_number']; ?> 
                                 </p>
                                 <a href="<?php echo $book['link']; ?>" class="btn btn-primary">Read more</a>
                             </div>
