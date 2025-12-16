154f589d65c04ddb81590708a1166df12fed2d5c
Alin Rosca <alin.rosca545@yahoo.com>
2025-12-16 15:36:54 +0200

    Cresterea interoperabilitatii prin setarea parametrilor sau argumentelor functiei la valoarea 0 default

--- functii/functie_suma3numere.php
+++ functii/functie_suma3numere.php
@@ -1,5 +1,5 @@
 <?php
-    function suma3numere($x, $y, $z) {
+    function suma3numere($x = 0, $y = 0, $z = 0) { // daca functia nu primeste parametrii sau primeste mai putin de 3 parametrii logica de executie nu este afectata, interoperabilitatea creste
         echo "Suma celor 3 numere $x, $y si $z este " . ($x + $y + $z);
     }
 ?>
