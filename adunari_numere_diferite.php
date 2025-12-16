168202e9343a65de61bdf3c326483a5610c872a6
Alin Rosca <alin.rosca545@yahoo.com>
2025-12-16 14:47:14 +0200

    vefificarea simultana pentru valorile setate data de: citirea primului set de date si conversia acestuia la int, citirea celui de al doilea set de date si conversia acestuia la int si afisarea rezultatelor adunarii numerelor simultan pe mai multe linii

---
+++ adunari_numere_diferite.php
@@ -0,0 +1,22 @@
+<?php
+    //vefificarea simultana pentru valorile setate
+    if (isset($_GET['a'], $_GET['b'], $_GET['c'], $_GET['x'], $_GET['y'], $_GET['z'])) {
+
+        //citirea primului set de date si conversia acestuia la int
+        $a = (int) $_GET['a'];
+        $b = (int) $_GET['b'];
+        $c = (int) $_GET['c'];
+
+        //citirea celui de al doilea set de date si conversia acestuia la int
+        $x = (int) $_GET['x'];
+        $y = (int) $_GET['y'];
+        $z = (int) $_GET['z'];
+
+        //afisarea rezultatelor adunarii numerelor simultan pe mai multe linii
+        echo "a + b = " . ($a + $b) .
+                "<br> c + x = " . ($c + $x) .
+                "<br> y + z = " . ($y + $z);
+    } else {
+        echo 'Te rog frumos sa introduci date valide in adresa URL exemplu a=1&b=2&c=3&x=4&y=5&z=6.';
+    }
+?>
