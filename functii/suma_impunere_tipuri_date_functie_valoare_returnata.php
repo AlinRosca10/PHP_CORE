cfa02c2fc1c1815e0707c203a68d27847dcc01a0
Alin Rosca <alin.rosca545@yahoo.com>
2025-12-16 18:46:16 +0200

    impunerea tipului de date pentru valoarea returnata

---
+++ functii/suma_impunere_tipuri_date_functie_valoare_returnata.php
@@ -0,0 +1,17 @@
+<?php
+    function suma(int $x, int $y) : int {
+        return $x + $y;
+    }
+    echo suma(5,8);
+
+    echo "<br>";
+
+    $x = 20;
+    $y = 'a';
+    if (is_int($x) && is_int($y)) { //validare
+        echo Suma($x, $y);
+    } else {
+        echo 'Va rog frumos sa introduceti date numerice intregi (de tipul int) valide';
+    }
+    //Suma('aaa', 10);//fatal error
+?>
